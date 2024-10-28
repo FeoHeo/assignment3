cf2ccc07886b6d5d2d253ce27471a340a21f059e
FeoHeo <nguyenduong8bc1@gmail.com>
2024-10-28 13:20:53 +1030

    fixed concurrency

--- assignment3.java
+++ assignment3.java
@@ -1,6 +1,5 @@
 import java.io.*;
 import java.net.*;
-import java.util.Scanner;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -9,26 +8,29 @@ public class assignment3 {
     private static int PORT = 12345;
     private static final int BUFFER_SIZE = 1024;
     private static final int MAX_CONNECTIONS = 10;
-    private static final String FILENAME_FORMAT = "book_%02d.txt";
+    private static final String FILENAME_FORMAT = "book_%2d.txt";
+    private static final String FILENAME_FORMAT_ALT = "book_%02d.txt";
 
     static class ListNode {
         String line;
         ListNode nextNode;       // Link to the next element in the shared list
         ListNode bookNextNode;   // Link to the next item in the same book
+        int bookNum;            // The id used to track book
 
-        ListNode(String line) {
+        ListNode(String line , int number) {
             this.line = line;
-        }
+            this.bookNum = number;
+        };
     }
 
     static class SharedList {
         ListNode head;             // Head of the linked list for each book
         Lock lock;                 // Lock for thread-safe access
-        int bookCounter;           // Track the book number
+        int bookTotal;           // Track the amount of book
 
         SharedList() {
             this.head = null;
-            this.bookCounter = 1;
+            this.bookTotal = 1;
             this.lock = new ReentrantLock();
         }
     }
@@ -50,7 +52,7 @@ public class assignment3 {
 
                 sharedList.lock.lock();
                 try {
-                    bookNumber = sharedList.bookCounter++;
+                    bookNumber = sharedList.bookTotal++;
                 } finally {
                     sharedList.lock.unlock();
                 }
@@ -59,18 +61,41 @@ public class assignment3 {
                 while ((line = reader.readLine()) != null) {
                     addToList(sharedList, line, bookNumber);
                 }
-
+                
+                
                 // Write to file
-                String filename = String.format(FILENAME_FORMAT, bookNumber);
+                String filename;
+                if(bookNumber >= 10) {
+                    filename = String.format(FILENAME_FORMAT, bookNumber);
+                } else {
+                    filename = String.format(FILENAME_FORMAT_ALT, bookNumber);
+                }
                 try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
+
                     ListNode currentNode = sharedList.head;
                     while (currentNode != null) {
-                        writer.write(currentNode.line);
-                        writer.newLine();
-                        currentNode = currentNode.bookNextNode;
+                        if(currentNode.bookNum == bookNumber) {
+                            writer.write(currentNode.line + "   $" + currentNode.bookNum);
+                            writer.newLine();
+                        }
+                        currentNode = currentNode.nextNode;
                     }
+                    
                 }
-
+                
+                // Write out the list
+                try(BufferedWriter wrt = new BufferedWriter(new FileWriter("Total.txt"))) {
+                    ListNode curr = sharedList.head;
+                    while(curr != null) {
+                        wrt.write(curr.line + "  $" + curr.bookNum);
+                        wrt.newLine();
+                        curr = curr.nextNode;
+                    }
+                } catch (Exception e) {
+                    // TODO: handle exception
+                }
+                
+                
             } catch (IOException e) {
                 e.printStackTrace();
             } finally {
@@ -83,12 +108,12 @@ public class assignment3 {
         }
     }
     
-
+    
     private static void addToList(SharedList sharedList, String line, int bookNumber) {
         sharedList.lock.lock();
         try {
-            ListNode newNode = new ListNode(line);
-
+            ListNode newNode = new ListNode(line , bookNumber);
+            
             if (sharedList.head == null) {
                 sharedList.head = newNode;  // If it's the first node, set as head
             } else {
@@ -97,33 +122,31 @@ public class assignment3 {
                     currentNode = currentNode.nextNode;
                 }
                 currentNode.nextNode = newNode; // Add to the end of the list
-            }
 
-            // Update bookNextNode pointers
-            if (bookNumber > 1) {
-                ListNode currentNode = sharedList.head;
-                while (currentNode != null && currentNode.bookNextNode != null) {
+                currentNode = sharedList.head;
+                while (currentNode.bookNextNode != null) {   // Iterate to where bookNextNode is null
+                    System.out.println("bookNextNode adress: " + currentNode.bookNextNode.toString());
+                    System.err.println("currNode content: " + currentNode.line);
                     currentNode = currentNode.bookNextNode;
                 }
-                if (currentNode != null) {
+                if (currentNode.bookNum == bookNumber) { // Check if it belongs to the same book
                     currentNode.bookNextNode = newNode; // Link to the book's nodes
                 }
             }
+            
+            // Update bookNextNode pointers
+            //if (bookNumber > 1) {
+            //}
 
+
+                
+            
             System.out.println("Added: " + line);
         } finally {
             sharedList.lock.unlock();
         }
     }
 
-    public static void prompt(String[] input) {
-        Scanner scn = new Scanner(System.in);
-        System.out.println("Input: ");
-
-        String receievd = scn.nextLine();
-        System.out.println("Entered: "+receievd);
-
-    }
 
     public static void startServer(SharedList sharedList) {
         try (ServerSocket serverSocket = new ServerSocket(PORT)) {
